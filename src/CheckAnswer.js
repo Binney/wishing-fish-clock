b7a9009064342f9df194a6ea700a884ee89bd76a
Sarah Binney <sar.binney@hotmail.co.uk>
2021-03-14 13:05:49 +0000

    Actual answer checker (spoilers!)

--- src/CheckAnswer.js
+++ src/CheckAnswer.js
@@ -1,16 +1,22 @@
-import sha256 from 'simple-sha256';
+import crypto from 'crypto';
 
-// TODO hash #420
+// Spoilers!
 const answers = {
-    'NORTH': 'NORTH',
-    'EAST': 'EAST',
-    'SOUTH': 'SOUTH',
-    'WEST': 'WEST'
+    'NORTH': '01ac41d9bbf4fe2ba84aab31761fee4b71036964daf6bb4eb686d828f6279c0c',
+    'EAST': '919ef96342bc9e72b8b2c6a8abf99383acb49bd1abcb84c3fd41d1dfca0e4803',
+    'SOUTH': '6747016a0c7722fcb274d75be41efa666cc984abde1ef1cff1e3c927435c3ce8',
+    'WEST': '3cb670f7a0733f613c6fde8bb0162b1c59714dcfe17fff87e51e0122e6e54e58'
+}
+
+const hash = async (buf) => {
+    return crypto.createHash('sha256')
+        .update(buf)
+        .digest('hex')
 }
 
 const checkAnswer = async (answer, orientation) => {
-    console.log("Checking " + answer + " against " + answers[orientation]);
-    return answers[orientation] === answer;
+    const hashedAnswer = await hash(answer);
+    return answers[orientation] === hashedAnswer;
 }
 
 export default checkAnswer;
